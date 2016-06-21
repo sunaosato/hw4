09b63fa068de7ba81e38f9e1ae870f1818d6e9d7
STEPER <step@step>
2016-06-21 11:14:58 +0900

    could run

--- pagerank.cpp
+++ pagerank.cpp
@@ -6,53 +6,102 @@
 #include <stdlib.h>
 
 using namespace std;
-#define SIZE 20
+#define SIZE 1000
+
+//value val[SIZE];
 
 
-ifstream fin("homework4/small_data.txt");
+ifstream fin("homework4/medium_data.txt");
 
-typedef struct value{
+class value{
+public:
   string name;
   // char neighbar[SIZE];
-  vector<value> neighbar;
+  vector<value*> neighbar;
   double score ;
   double pro_score ;
-}value;
+};
 
-int insert_map(int vertex_num){
+int search(string name,int vertex_num,value *val){
+  int num = 0;
+  while(num < vertex_num ){
+    if(name == val[num].name){
+      break;
+    }else{
+      num++;
+    }
+  }
+  return num;
+}
+
+void insert(int vertex_num,value *val){
   //  map<std::string, value> pagerank_Map;
   string num,name,edge_from,edge_to;
-  int edge_num;
-  value val[SIZE];
+  int total_edge;
+
  
   for(int i=0;i < vertex_num;i++){
     getline(fin,name);
     val[i].name = name;
+    val[i].score = 100.0;
+    val[i].pro_score = 0.0;
   }
-
+ 
   getline(fin,num);
-  edge_num = atoi(num.c_str());
-   cout << edge_num<<endl;
-  for(int j=0;j< edge_num;j++){
-      
+  total_edge = atoi(num.c_str());
+  // cout << total_edge<<endl;
+
+  for(int j=0;j< total_edge;j++){
+    
     getline(fin,edge_from,' ');
-    cout<<"From:"<<edge_from<<endl;
+    //  cout<<"From:"<<edge_from<<endl;
 
-    
     getline(fin,edge_to);
-    cout<<"To:"<<edge_to<<endl;
-    
-    /*   for(int k=0;k< vertex_num;k++){
+    // cout<<"To:"<<edge_to<<endl;
 
-      
-	 }*/
+    int from = 0,to = 0;
+    from = search(edge_from,vertex_num,val);
+    to = search(edge_to,vertex_num,val);
+    
+    val[from].neighbar.push_back(&val[to]);
   }
-     
-  return 0;
 }
+ 
+
+
+
+void pagerank(value* val,int vertex_num){
+
+  for (int i=0;i< vertex_num;i++){
+    int neighbar_num = val[i].neighbar.size();
+    int share = val[i].score/neighbar_num;
+    // cout<<val[i].neighbar.size()<<","<<share<<endl;
+    
+    for(int j =0;j < neighbar_num;j++){
+      val[i].neighbar[j]->pro_score += share;
+    }
+  }
+
+   int total=0;
+    for(int l=0;l< vertex_num;l++){
+     
+
+    val[l].score = val[l].pro_score;
+    total += val[l].score;
+    val[l].pro_score =0.0;
+    cout<<val[l].name<<"'s score:"<<val[l].score<<endl;
+    }
+    cout<<"total:"<<total<<endl;
+    cout<<"true value:"<<vertex_num*100.0<<endl;
+    
+}
+
+
+
 
 int main(){
 
+  value val[SIZE];
   int vertex_num;
   string str;
 
@@ -64,13 +113,17 @@ int main(){
  getline(fin,str);
  vertex_num = atoi(str.c_str());
  
- cout << vertex_num<<endl;
+  cout << vertex_num<<endl;
 
- insert_map(vertex_num);
+  insert(vertex_num,val);
+
+  for(int i=0;i < 3;i++){
+   cout<<i+1<<"回目"<<endl;
+ pagerank(val,vertex_num);
+ }
 
  fin.close();
 
   return 0;
 
 }
-
